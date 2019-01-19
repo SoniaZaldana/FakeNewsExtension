36911d2bf58367364b108326888c0250f081df5b
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2019-01-19 10:24:05 -0500

    Finished fakenews and summarization API's

--- fakenews.py
+++ fakenews.py
@@ -2,7 +2,9 @@ import requests
 
 # API URL
 # You can change 'summarize' to different endpoints: sentiment, keywords, etc.
-api_url = "https://www.summarizebot.com/api/checkfake?apiKey=7bc4b0c77ace418290dc797690f87425&size=20&keywords=10&fragments=15&url=URL_FOR_PROCESSING"
+api_url = "https://www.summarizebot.com/api/checkfake?apiKey=7bc4b0c77ace418290dc797690f87425&" \
+          "size=20&keywords=10&fragments=15" \
+          "&url=https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628"
 r = requests.get(api_url)
 json_res = r.json()
 print(json_res)
--- summarization.py
+++ summarization.py
@@ -2,7 +2,11 @@ import requests
 
 # API URL
 # You can change 'summarize' to different endpoints: sentiment, keywords, etc.
-api_url = "https://www.summarizebot.com/api/summarize?apiKey=7bc4b0c77ace418290dc797690f87425&size=20&keywords=10&fragments=15&url=URL_FOR_PROCESSING"
+api_url = "https://www.summarizebot.com/api/summarize?apiKey=7bc4b0c77ace418290dc797690f87425&size=20" \
+          "&keywords=10" \
+          "&fragments=15" \
+          "&url=https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628"
 r = requests.get(api_url)
+print(r.status_code)
 json_res = r.json()
 print(json_res)
