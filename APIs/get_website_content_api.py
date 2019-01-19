7213b48fe8132b5d8156d9ff3559fad92be4c9db
Nicolae Stroncea <stroncea.nicolae@gmail.com>
2019-01-19 13:00:08 -0500

    get the content of any websit

--- APIs/get_website_content_api.py
+++ APIs/get_website_content_api.py
@@ -1,7 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-r=requests.get("https://www.bbc.com/news/world-us-canada-46928440",
-               headers={"Content-Type": "application/json",
-                       "x-api-key": "bajpVjk1LsymV1FT6mmiqINB4UGRULDhdiR2f2ze"})
+r = requests.get("https://www.bbc.com/news/world-us-canada-46928440",
+                 headers={"Content-Type": "application/json",
+                          "x-api-key": "bajpVjk1LsymV1FT6mmiqINB4UGRULDhdiR2f2ze"})
+url = "https://www.reuters.com/article/us-myanmar-journalists/myanmar-court-rejects-appeal-by-jailed-reuters-reporters-idUSKCN1P50HL"
+r = requests.get("http://boilerpipe-web.appspot.com/extract?url=" + url+"?output=text")
 
-print(r.content)
+soup = BeautifulSoup(r.content)
+print(soup.get_text())
