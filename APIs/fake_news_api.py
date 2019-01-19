4632783592332ff0feaaa40db40a45ce0e870969
Elliot Ford <webelliot@fordcochrane.com>
2019-01-19 14:00:32 -0500

    Rewrote fake_news_api.py to be more dynamic

--- APIs/fake_news_api.py
+++ APIs/fake_news_api.py
@@ -1,10 +1,16 @@
 import requests
+from passwords import SUMMARIZE_BOT_KEY
 
-# API URL
-# You can change 'summarize' to different endpoints: sentiment, keywords, etc.
-api_url = "https://www.summarizebot.com/api/checkfake?apiKey=&" \
-          "size=20&keywords=10&fragments=15" \
-          "&url=https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628"
-r = requests.get(api_url)
-json_res = r.json()
-print(json_res)
+API_URL = "https://www.summarizebot.com/api/checkfake?apiKey=" + SUMMARIZE_BOT_KEY + \
+          "&size=20&keywords=10&fragments=15" \
+          "&url="
+
+def determine_fakeness(url):
+    # You can change 'summarize' to different endpoints: sentiment, keywords, etc.
+    response = requests.get(API_URL + url)
+    json_res = response.json()
+    return json_res
+
+if __name__ == "__main__":
+    fakeness = determine_fakeness("https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628")
+    print(fakeness)
