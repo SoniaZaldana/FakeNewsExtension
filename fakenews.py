26601e58fc58cc5a724fcd6b140db3671f37dc08
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2019-01-19 10:39:41 -0500

    Deleted my API keys

--- fakenews.py
+++ fakenews.py
@@ -2,7 +2,7 @@ import requests
 
 # API URL
 # You can change 'summarize' to different endpoints: sentiment, keywords, etc.
-api_url = "https://www.summarizebot.com/api/checkfake?apiKey=7bc4b0c77ace418290dc797690f87425&" \
+api_url = "https://www.summarizebot.com/api/checkfake?apiKey=&" \
           "size=20&keywords=10&fragments=15" \
           "&url=https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628"
 r = requests.get(api_url)
--- summarization.py
+++ summarization.py
@@ -2,7 +2,7 @@ import requests
 
 # API URL
 # You can change 'summarize' to different endpoints: sentiment, keywords, etc.
-api_url = "https://www.summarizebot.com/api/summarize?apiKey=7bc4b0c77ace418290dc797690f87425&size=20" \
+api_url = "https://www.summarizebot.com/api/summarize?apiKey=&size=20" \
           "&keywords=10" \
           "&fragments=15" \
           "&url=https://politics.theonion.com/john-kelly-resigns-in-last-ditch-effort-to-save-his-and-1830989628"
